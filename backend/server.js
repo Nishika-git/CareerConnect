15f2a23307c460efe0d461d6fcb32384aa2bf716
Nishika-git <nishikalab@gmail.com>
2026-07-23 12:03:27 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import postRoutes from "./routes/posts.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+
+dotenv.config();
+
+const app= express();
+
+app.use(cors());
+app.use(express.json());
+app.use(postRoutes)
+app.use(userRoutes)
+app.use(express.static("uploads"))
+
+
+const start = async () =>{
+    const connectDB = await mongoose.connect("mongodb+srv://nishika:nishika@careerconnect.qdigb5w.mongodb.net/?appName=CareerConnect");
+
+    app.listen(9080, () =>{
+        console.log("Server is running on port 9080");
+    })
+}
+
+start();
