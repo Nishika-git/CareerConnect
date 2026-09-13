0e749a3702b3872b54779210cb139bd70e42b3f4
Nishika-git <nishikalab@gmail.com>
2026-09-13 20:31:29 +0530

    root

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:9080"
+export const BASE_URL = " https://careerconnect-q0rr.onrender.com"
 
 const clientServer = axios.create({
     baseURL: BASE_URL,
